1f7f03debe682d068bae0163d3fd06827aa32bf6
tum0xa <timofey.samodurov@mail.ru>
2019-10-02 11:04:29 +0500

    class Server was created

---
+++ server/server.py
@@ -0,0 +1,31 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+from server.settings import *
+
+
+class Server:
+
+    def __init__(self, address=DEFAULT_ADDRESS, port=DEFAULT_PORT):
+        self.address = address
+        self.port = port
+
+    def connect(self):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind((self.address, self.port))
+        self.socket.listen(5)
+
+    def run(self):
+        while True:
+            client, address = self.socket.accept()
+            print(f'Request for connection from {address} ')
+            client.send('I see you'.encode('ascii'))
+            client.close()
+            
+    def send_message(self):
+        pass
+
+    def recieve_message(self):
+        pass
+
+    def handle_message(self, message):
+        pass
---
+++ server/settings.py
@@ -0,0 +1,4 @@
+# Server
+# General settings
+DEFAULT_ADDRESS = '127.0.0.1'
+DEFAULT_PORT = 8888
